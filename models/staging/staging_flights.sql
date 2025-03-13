a2b2dc72ac6a802022e5f0caa1fe2a74ca1fbdb3
Gary <garylin1118@GarydeMacBook-Air.local>
2025-03-13 15:07:45 +0100

    changed the flight table name

--- models/staging/staging_flights.sql
+++ models/staging/staging_flights.sql
@@ -2,7 +2,7 @@
     
     WITH flights AS (
         SELECT * 
-        FROM {{source('flights_data', 'flights')}}
+        FROM {{source('flights_data', 'flights_05_07')}}
         --WHERE DATE_PART('month', flight_date) = 1 
     )
     SELECT * FROM flights
